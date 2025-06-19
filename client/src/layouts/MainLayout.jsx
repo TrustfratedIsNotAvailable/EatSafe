f6fdb9125add426a7a8f84784df9ac714f7153e8
TrustfratedIsNotAvailable <nusratmithila524@gmail.com>
2025-06-19 18:21:28 +0600

    banner done

--- client/src/layouts/MainLayout.jsx
+++ client/src/layouts/MainLayout.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Navbar from "../components/shared/Navbar";
+import { Outlet } from "react-router";
 
 const MainLayout = () => {
   return (
     <div>
       <Navbar />
       <div className="pt-16 p-4 flex flex-col items-center">
-        <h1 className="text-xl font-semibold">Hello World</h1>
-        <p>This is your main app content.</p>
+        <Outlet />
       </div>
     </div>
   );
--- client/src/routes/router.jsx
+++ client/src/routes/router.jsx
@@ -2,21 +2,27 @@ import { createBrowserRouter } from "react-router";
 import MainLayout from "../layouts/MainLayout";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
+import Home from "../pages/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
   },
   {
-    path:"login",
-    element:<Login />
+    path: "login",
+    element: <Login />,
   },
   {
-    path:"register",
-    element:<Register />
-  }
-   
+    path: "register",
+    element: <Register />,
+  },
 ]);
 
 export default router;
